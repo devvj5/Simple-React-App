14ef9318db76337e12985a82b4a088d0bcc56332
Vinit Joshi <vinit@5exceptions.com>
2021-12-14 17:39:38 +0530

    add comment

--- src/form.js
+++ src/form.js
@@ -1,32 +1,53 @@
 import React, { useState } from 'react'
 
 export default function Form() {
-    const [firstName, setFirstName] = useState('')
-    const [lastName, setLastName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [firstNameVal, setFirstNameVal] = useState(false)
-    const [lastNameVal, setLastNameVal] = useState(false)
-    const [emailVal, setEmailVal] = useState(false)
-    const [passwordVal, setPasswordVal] = useState(false)
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [firstNameVal, setFirstNameVal] = useState(false);
+    const [lastNameVal, setLastNameVal] = useState(false);
+    const [emailVal, setEmailVal] = useState(false);
+    const [passwordVal, setPasswordVal] = useState(false);
 
+    /**
+     * function: handleSubmit
+     * description: handle the form submission
+     * @param {*} e 
+     * Author: Vinit Joshi
+     */
     const handleSubmit = (e) => {
         e.preventDefault()
         if (firstName.length < 1) {
-            setFirstNameVal(true); setLastNameVal(false); setEmailVal(false); setPasswordVal(false);
+            setFirstNameVal(true); 
+            setLastNameVal(false); 
+            setEmailVal(false); 
+            setPasswordVal(false);
         }
         else if (lastName.length < 1) {
-            setFirstNameVal(false); setLastNameVal(true); setEmailVal(false); setPasswordVal(false);
+            setFirstNameVal(false); 
+            setLastNameVal(true); 
+            setEmailVal(false); 
+            setPasswordVal(false);
         }
         else if (email.length < 1) {
-            setFirstNameVal(false); setLastNameVal(false); setEmailVal(true); setPasswordVal(false);
+            setFirstNameVal(false); 
+            setLastNameVal(false); 
+            setEmailVal(true); 
+            setPasswordVal(false);
         }
         else if (password.length < 1) {
-            setFirstNameVal(false); setLastNameVal(false); setEmailVal(false); setPasswordVal(true);
+            setFirstNameVal(false); 
+            setLastNameVal(false);
+            setEmailVal(false); 
+            setPasswordVal(true);
         }
         else {
 
-            setFirstNameVal(false); setLastNameVal(false); setEmailVal(false); setPasswordVal(false);
+            setFirstNameVal(false);
+            setLastNameVal(false);
+            setEmailVal(false);
+            setPasswordVal(false);
 
             let data = {
                 firstName: firstName,
