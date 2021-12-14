b3ba06906e4ef22f8cffb4b6cad98b5b70d55abf
Vinit Joshi <vinit@5exceptions.com>
2021-12-14 17:02:59 +0530

    init code

---
+++ src/form.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react'
+
+export default function Form() {
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [firstNameVal, setFirstNameVal] = useState(false)
+    const [lastNameVal, setLastNameVal] = useState(false)
+    const [emailVal, setEmailVal] = useState(false)
+    const [passwordVal, setPasswordVal] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (firstName.length < 1) {
+            setFirstNameVal(true); setLastNameVal(false); setEmailVal(false); setPasswordVal(false)
+        }
+        else if (lastName.length < 1) {
+            setFirstNameVal(false); setLastNameVal(true); setEmailVal(false); setPasswordVal(false)
+        }
+        else if (email.length < 1) {
+            setFirstNameVal(false); setLastNameVal(false); setEmailVal(true); setPasswordVal(false)
+        }
+        else if (password.length < 1) {
+            setFirstNameVal(false); setLastNameVal(false); setEmailVal(false); setPasswordVal(true)
+        }
+        else {
+            let data = {
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                password: password
+            }
+            console.log(data)
+        }
+
+    }
+    return (
+        <div className="form">
+            <form className="form-res" onSubmit={(e) => handleSubmit(e)}>
+                <div className='title'>FORM</div>
+                <div >
+                    <div className='form-div'>
+                        <label htmlFor="firstname">First Name:</label>
+                        <div>
+                            <input id="firstname" type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)}></input>
+                            {firstNameVal && <div><small>*required</small></div>}
+                        </div>
+                    </div>
+                    <div className='form-div'>
+                        <label htmlFor="lastname"> Last Name:</label>
+                        <div>
+                            <input id="lastname" type="text" value={lastName} onChange={(e) => setLastName(e.target.value)}></input>
+                            {lastNameVal && <div><small>*required</small></div>}
+                        </div>
+                    </div>
+                    <div className='form-div'>
+                        <label htmlFor="email">Email:</label>
+                        <div>
+                            <input id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
+                            {emailVal && <div><small>*required</small></div>}
+                        </div>
+                    </div>
+                    <div className='form-div'>
+                        <label htmlFor="password">Password:</label>
+                        <div>
+                            <input id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
+                            {passwordVal && <div><small>*required</small></div>}
+                        </div>
+                    </div>
+                </div>
+                <button type="submit" className='button'>Submit</button>
+            </form>
+        </div>
+    )
+}
